e452d061112e0014cfac6d3ce20dd438c9fbc92e
bugCan <869623380@qq.com>
2022-12-17 23:10:50 +0800

    initial project

---
+++ src/main/java/cn/xeonscan/mp/MybatisPlusTest1217Application.java
@@ -0,0 +1,13 @@
+package cn.xeonscan.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusTest1217Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusTest1217Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xeonscan/mp/MybatisPlusTest1217ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xeonscan.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusTest1217ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
